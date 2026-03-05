8ae755bea083e5325ebcdb75dbc184c532099466
AgustinBlack <agustinblack04@gmail.com>
2026-03-05 13:36:04 -0300

    Add some new DB

--- src/components/Rutine/Rutine.jsx
+++ src/components/Rutine/Rutine.jsx
@@ -1,5 +1,12 @@
 import { useState } from "react"
 import exercises from "../../data/exercises.json"
+import rutineExercises from "../../data/rutineExercises.json"
+import rutines from "../../data/rutines.json"
+import userRutines from "../../data/userRutines.json"
+import users from "../../data/users.json"
+import workoutExercises from "../../data/workoutExercises.json"
+import workoutSessions from "../../data/workoutSessions.json"
+import WorkoutCalendar from "../CalendarWorkout/CalendarWorkout"
 
 const Rutine = () => {
     const [query, setQuery] = useState("")
@@ -10,9 +17,81 @@ const Rutine = () => {
         exercise.name.toLowerCase().includes(query.toLowerCase())
     )
 
+    
+    const rutine = rutines.find(r => r.id === 1)
+    
+    const rutineExerciseList = rutineExercises.filter((ex) => ex.rutineId === rutine.id)
+
+    const days = {}
+
+    rutineExerciseList.forEach((ex) => {
+        if (!days[ex.day]) {
+            days[ex.day] = []
+        }
+
+        const excerciseInfo =  exercises.find(e => e.id === ex.exerciseId)
+        days[ex.day].push({
+            ...ex,
+            exercise: excerciseInfo})
+    })
+
+    console.log(days)
+
     return (
         <>
             <h1>Rutina</h1>
+
+            {/* <p>Agregar:
+                Cuadro que muestre rutina completa por dias.
+                <br />
+                calendario que muestre que dia de la rutina toca en cada fecha y si esa fecha ya paso que se ponga con color verde si fue a entrenar y rojo si no.
+                <br />
+                Espacio para que el cliente agregue comentarios sobre la rutina.
+                <br />
+                Crear un nuevo componente que filtre el dia de la rutina que toca en el dia de la fecha y lo mustre con la opcion para que coplete cuantas series y reps hizo y con cuanto peso. 
+                <br />
+                Boton para modificar la rutina (solo lo vamos a podes hacer los PF).
+                <br />
+                Agregar al costado de cada ejercicio de la rutina un boton para que el cliente vea el video del ejercicio en caso de necesitar y aparte tres opciones para elegir bien - masomenos - mal, la idea es que selecciones una de las tres en base a como se sintio haciendo ese ejercicio.
+            </p> */}
+
+            <>
+            <h1>{rutine.name}</h1>
+
+            <WorkoutCalendar workoutSessions={workoutSessions} />
+
+            {Object.keys(days).map((day) => (
+            <div key={day} style={{ marginBottom: "30px" }}>
+                
+                <h2>Día {day}</h2>
+
+                {days[day].map((ex) => (
+                <div key={ex.id} style={{
+                    border: "1px solid #ccc",
+                    padding: "10px",
+                    marginBottom: "10px"
+                }}>
+
+                    <h3>{ex.exercise.name}</h3>
+
+                    <p>
+                    {ex.sets} sets x {ex.reps}
+                    </p>
+
+                    <p>Descanso: {ex.rest} segundos</p>
+
+                    <img
+                    src={ex.exercise.image}
+                    alt={ex.exercise.name}
+                    width="150"
+                    />
+
+                </div>
+                ))}
+
+            </div>
+            ))}
+            </>
 
             <div style={{ position: "relative", width: "300px" }}>
                 <input
---
+++ src/components/Us/Us.jsx
@@ -0,0 +1,15 @@
+const Us = () => {
+    return (
+        <div>
+            <h1>Nosotros</h1>
+            <p>
+                Somos un grupo de entusiastas del fitness y el desarrollo web que se unieron para crear Training Black, una aplicación diseñada para ayudarte a alcanzar tus objetivos de entrenamiento de manera efectiva y personalizada. Nuestra misión es proporcionar a los usuarios una plataforma intuitiva y completa que les permita planificar, seguir y mejorar su rutina de ejercicios, así como llevar un registro de sus progresos y objetivos.
+            </p>
+            <p>
+                En Training Black, creemos que el fitness es para todos, independientemente de su nivel de experiencia o condición física. Por eso, nos esforzamos por ofrecer una amplia variedad de ejercicios, rutinas personalizables y herramientas de seguimiento que se adapten a las necesidades individuales de cada usuario. Nuestro equipo está comprometido con la calidad y la innovación, siempre buscando formas de mejorar la experiencia del usuario y brindar el mejor soporte posible.
+            </p>
+        </div>
+    )
+}
+
+export default Us
